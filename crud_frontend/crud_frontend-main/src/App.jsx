f9c2f5462da8c02f6a60f3b1da3f14a72ba3189d
veerraju5a <68723979+veerraju5a@users.noreply.github.com>
2025-08-06 16:35:46 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://54.89.59.171:9090/springapp1';
+const BASE_URL = 'http://98.80.244.251:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
